a460fe755b0f72d6326c4d8816c693ca28d68546
SERT <SERT>
2020-01-13 17:13:21 -0800

    uncommented stuff

--- src/main/kotlin/org/sert2521/infiniterecharge2020/colorwheelspin/ColorWheelSpin.kt
+++ src/main/kotlin/org/sert2521/infiniterecharge2020/colorwheelspin/ColorWheelSpin.kt
@@ -1,14 +1,14 @@
 package org.sert2521.infiniterecharge2020.colorwheelspin
 
-//import com.revrobotics.ColorSensorV3
-//import edu.wpi.first.wpilibj.I2C
+import com.revrobotics.ColorSensorV3
+import edu.wpi.first.wpilibj.I2C
 import org.sert2521.infiniterecharge2020.MotorControllers
 import org.sert2521.sertain.motors.MotorController
 import org.sert2521.sertain.subsystems.Subsystem
 
 class ColorWheelSpin : Subsystem("ColorWheelSpin") {
-    //private val i2cPort: I2C.Port = I2C.Port.kOnboard
-    //val sensor = ColorSensorV3(i2cPort)
+    private val i2cPort: I2C.Port = I2C.Port.kOnboard
+    val sensor = ColorSensorV3(i2cPort)
 
     private val extender = MotorController(MotorControllers.extender)
     private val spinner = MotorController(MotorControllers.spinner)
