f5f80c4bbf55c7220ae61fe31dc9f112eb328fe6
SERT <SERT>
2020-01-14 18:36:51 -0800

    made a master function that connects all the others

--- src/main/kotlin/org/sert2521/infiniterecharge2020/Main.kt
+++ src/main/kotlin/org/sert2521/infiniterecharge2020/Main.kt
@@ -1,36 +1,23 @@
 package org.sert2521.infiniterecharge2020
 
 import com.revrobotics.ColorSensorV3
-import edu.wpi.first.wpilibj.util.ColorShim
 import edu.wpi.first.wpilibj.I2C
 import org.sert2521.sertain.coroutines.periodic
 import org.sert2521.sertain.events.onTeleop
 import org.sert2521.sertain.robot
+import edu.wpi.first.wpilibj.util.Color
 
 suspend fun main() = robot {
     onTeleop {
         val sensor = ColorSensorV3(I2C.Port.kOnboard)
         periodic(20) {
-            val data = sensor.rawColor
-            var red = 0
-            var green = 0
-            var blue = 0
-            {
-                val mag = data.red + data.green + data.blue
-                red = data.red / mag
-                green = data.green / mag
-                blue = data.blue / mag
-
-            }
+            val color: Color = sensor.color
             print("red: ")
-            print(red)
-            println("%")
+            println(color.red)
             print("green: ")
-            print(green)
-            println("%")
+            println(color.green)
             print("blue: ")
-            print(blue)
-            println("%")
+            println(color.blue)
         }
     }
 }
