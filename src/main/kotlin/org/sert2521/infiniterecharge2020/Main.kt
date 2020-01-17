c6ec019ac6af3c65796f8bac8c5c2dd9ddfaaf95
SERT <SERT>
2020-01-16 18:17:38 -0800

    removed unused imports and clean up

--- src/main/kotlin/org/sert2521/infiniterecharge2020/Main.kt
+++ src/main/kotlin/org/sert2521/infiniterecharge2020/Main.kt
@@ -1,18 +1,9 @@
 package org.sert2521.infiniterecharge2020
 
-import com.revrobotics.ColorSensorV3
-import edu.wpi.first.wpilibj.I2C
 import edu.wpi.first.wpilibj.Joystick
-import edu.wpi.first.wpilibj.util.Color
 import org.sert2521.infiniterecharge2020.colorwheelspin.spinAndExtend
-import org.sert2521.infiniterecharge2020.colorwheelspin.spinForColors
-import org.sert2521.infiniterecharge2020.colorwheelspin.spinToColor
-import org.sert2521.sertain.coroutines.periodic
 import org.sert2521.sertain.coroutines.watch
-import org.sert2521.sertain.events.True
-import org.sert2521.sertain.events.onTeleop
 import org.sert2521.sertain.robot
-import kotlin.math.abs
 
 val primaryJoystick by lazy { Joystick(Operator.PRIMARY_CONTROLLER) }
 
