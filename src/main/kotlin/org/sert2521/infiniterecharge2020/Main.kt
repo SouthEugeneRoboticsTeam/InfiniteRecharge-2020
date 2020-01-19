3c9e46df2c691551e08ca714812cf5b6e38b66ae
Carkoth <kturner44@4j.lane.edu>
2020-01-19 15:09:11 -0800

    Fix gradle

--- src/main/kotlin/org/sert2521/infiniterecharge2020/Main.kt
+++ src/main/kotlin/org/sert2521/infiniterecharge2020/Main.kt
@@ -1,7 +1,10 @@
 package org.sert2521.infiniterecharge2020
 
+import kotlinx.coroutines.launch
 import org.sert2521.sertain.robot
 
 suspend fun main() = robot {
-    // Add robot code
+    launch {
+        println("Hello")
+    }
 }
