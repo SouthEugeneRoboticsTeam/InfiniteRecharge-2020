e1b99aa9958a79e1fb40df18ad585666c05c2a59
Aidan Heller <asfheller@gmail.com>
2020-02-02 17:56:19 -0800

    Progress

--- src/main/kotlin/org/sert2521/infiniterecharge2020/Main.kt
+++ src/main/kotlin/org/sert2521/infiniterecharge2020/Main.kt
@@ -3,13 +3,9 @@ package org.sert2521.infiniterecharge2020
 import edu.wpi.first.wpilibj.controller.RamseteController
 import edu.wpi.first.wpilibj.geometry.Pose2d
 import edu.wpi.first.wpilibj.geometry.Rotation2d
-import edu.wpi.first.wpilibj.geometry.Translation2d
-import edu.wpi.first.wpilibj.trajectory.Trajectory
 import edu.wpi.first.wpilibj.trajectory.TrajectoryConfig
 import edu.wpi.first.wpilibj.trajectory.TrajectoryGenerator
-import kotlinx.coroutines.launch
 import org.sert2521.infiniterecharge2020.drivetrain.Drivetrain
-import org.sert2521.infiniterecharge2020.drivetrain.Velocity
 import org.sert2521.infiniterecharge2020.drivetrain.kinematics
 import org.sert2521.infiniterecharge2020.drivetrain.runPath
 import org.sert2521.sertain.events.onEnable
@@ -20,13 +16,11 @@ import org.sert2521.sertain.subsystems.access
 import org.sert2521.sertain.subsystems.add
 import org.sert2521.sertain.subsystems.doTask
 import org.sert2521.sertain.subsystems.use
-import org.sert2521.sertain.units.MetricUnit
-import org.sert2521.sertain.units.MetricValue
 import org.sert2521.sertain.units.mps
-import kotlin.math.PI
 
 suspend fun main() = robot {
     println("Robot program starting")
+
     add<Drivetrain>()
 
     onEnable {
@@ -47,8 +41,8 @@ suspend fun main() = robot {
                 drivetrain.odometry.resetPosition(Pose2d(0.0, 0.0, Rotation2d(0.0)), Rotation2d(0.0))
                 val trajectory = TrajectoryGenerator.generateTrajectory(
                         Pose2d(0.0, 0.0, Rotation2d(0.0)),
-                        listOf(Translation2d(2.0, 1.0), Translation2d(4.0, -1.0)),
-                        Pose2d(5.0, 0.0, Rotation2d(0.0)),
+                        listOf(),
+                        Pose2d(2.0, 0.0, Rotation2d(0.0)),
                         TrajectoryConfig(1.0, 0.4).setKinematics(kinematics)
                 )
                 runPath(
--- src/main/kotlin/org/sert2521/infiniterecharge2020/drivetrain/DrivetrainPreferences.kt
+++ src/main/kotlin/org/sert2521/infiniterecharge2020/drivetrain/DrivetrainPreferences.kt
@@ -14,6 +14,6 @@ const val TICKS_PER_REVOLUTION = 4096
 val motorEncoder = Encoder(TICKS_PER_REVOLUTION)
 
 // TODO measure wheels
-val wheelRadius = 0.098425.m
+val wheelRadius = 0.095.m
 
 val kinematics = DifferentialDriveKinematics(0.59690)
