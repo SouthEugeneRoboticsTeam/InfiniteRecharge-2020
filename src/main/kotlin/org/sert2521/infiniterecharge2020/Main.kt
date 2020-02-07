adc2ad1de83afa4a31b0a524e6d13d63dfafbe2b
Carkoth <kturner44@4j.lane.edu>
2020-02-06 17:08:11 -0800

    remove main stuff

--- src/main/kotlin/org/sert2521/infiniterecharge2020/Main.kt
+++ src/main/kotlin/org/sert2521/infiniterecharge2020/Main.kt
@@ -1,22 +1,7 @@
 package org.sert2521.infiniterecharge2020
 
-import edu.wpi.first.wpilibj.Joystick
-import org.sert2521.infiniterecharge2020.climber.climberDown
-import org.sert2521.infiniterecharge2020.climber.climberUp
-import org.sert2521.sertain.coroutines.watch
 import org.sert2521.sertain.robot
 
-val primaryJoystick by lazy { Joystick(0) }
-
 suspend fun main() = robot {
-    { primaryJoystick.getRawButton(7) }.watch {
-        whileTrue {
-            climberUp()
-        }
-    };
-    { primaryJoystick.getRawButton(8) }.watch {
-        whileTrue {
-            climberDown()
-        }
-    }
+
 }
--- src/main/kotlin/org/sert2521/infiniterecharge2020/climber/Climber.kt
+++ src/main/kotlin/org/sert2521/infiniterecharge2020/climber/Climber.kt
@@ -1,8 +1,6 @@
 package org.sert2521.infiniterecharge2020.climber;
 
 import org.sert2521.infiniterecharge2020.MotorControllers
-import org.sert2521.infiniterecharge2020.MotorControllers.CLIMBER_WINCH_A
-import org.sert2521.infiniterecharge2020.MotorControllers.CLIMBER_WINCH_B
 import org.sert2521.sertain.motors.MotorController
 import org.sert2521.sertain.subsystems.Subsystem
 
