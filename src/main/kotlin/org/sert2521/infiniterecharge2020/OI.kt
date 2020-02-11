756c3bd011f516723e90a0c94f0ebf24fad7017f
William <whou125@gmail.com>
2020-02-10 18:54:48 -0800

    Add initial function to change driver camera view

--- src/main/kotlin/org/sert2521/infiniterecharge2020/OI.kt
+++ src/main/kotlin/org/sert2521/infiniterecharge2020/OI.kt
@@ -1,5 +1,6 @@
 package org.sert2521.infiniterecharge2020
 
+import edu.wpi.first.networktables.NetworkTableInstance
 import edu.wpi.first.wpilibj.Joystick
 import edu.wpi.first.wpilibj.XboxController
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser
@@ -10,6 +11,10 @@ import org.sert2521.sertain.telemetry.linkTableEntry
 object OI {
     enum class ControlMode {
         CONTROLLER, JOYSTICK
+    }
+
+    enum class DriverCameraSource(val key: String) {
+        FRONT("Front"), CLIMBER("Climber")
     }
 
     // TODO: Figure out why this isn't working
@@ -27,6 +32,10 @@ object OI {
     val primaryController by lazy { XboxController(Operator.PRIMARY_CONTROLLER) }
     val primaryJoystick by lazy { Joystick(Operator.PRIMARY_STICK) }
     val secondaryJoystick by lazy { Joystick(Operator.SECONDARY_STICK) }
+
+    fun setDriverCamera(source: DriverCameraSource) {
+        NetworkTableInstance.getDefault().getEntry("/current_camera").setString(source.key)
+    }
 }
 
 fun CoroutineScope.initControls() {
