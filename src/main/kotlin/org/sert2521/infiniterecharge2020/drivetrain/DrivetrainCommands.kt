277c9696dcc6117c1cfe0820b892f968a849a85e
Carkoth <kturner44@4j.lane.edu>
2020-02-24 17:30:41 -0800

    update align to ball

--- src/main/kotlin/org/sert2521/infiniterecharge2020/drivetrain/DrivetrainCommands.kt
+++ src/main/kotlin/org/sert2521/infiniterecharge2020/drivetrain/DrivetrainCommands.kt
@@ -60,7 +60,7 @@ suspend fun controlDrivetrain() = doTask {
     }
 }
 
-suspend fun alignToBall(offset: Double) = doTask {
+suspend fun alignToBall(offset: Double, oneside: Boolean = false) = doTask {
     val drivetrain = use<Drivetrain>()
     var visionLastAlive = TableEntry("last_alive", 0.0, "Vision")
     var visionAngle = TableEntry("xAngOff", 0.0, "Vision")
@@ -78,7 +78,11 @@ suspend fun alignToBall(offset: Double) = doTask {
             periodic(20) {
                 if (lastAlive != visionLastAlive.value) {
                     lastAlive = visionLastAlive.value + drivetrain.rawHeading
-                    lastAngle = visionAngle.value + drivetrain.rawHeading - (sign(visionAngle.value) * offset)
+                    if(oneside) {
+                        lastAngle = visionAngle.value + drivetrain.rawHeading - offset
+                    }else{
+                        lastAngle = visionAngle.value + drivetrain.rawHeading - (sign(visionAngle.value) * offset)
+                    }
                 }
 
                 val turnValue = controller.next(0.0, (drivetrain.rawHeading - lastAngle).IEEErem(360.0))
