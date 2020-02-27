8363bb33e511e0a1f99c1deb85de7719e68e83e0
William <whou125@gmail.com>
2020-02-27 09:43:17 -0800

    Use non-sertain version of linkTableEntry

--- src/main/kotlin/org/sert2521/infiniterecharge2020/utils/Utils.kt
+++ src/main/kotlin/org/sert2521/infiniterecharge2020/utils/Utils.kt
@@ -1,7 +1,11 @@
 package org.sert2521.infiniterecharge2020.utils
 
 import kotlin.math.sign
+import kotlinx.coroutines.CoroutineScope
 import org.sert2521.sertain.control.PidfConfig
+import org.sert2521.sertain.events.onTick
+import org.sert2521.sertain.telemetry.Table
+import org.sert2521.sertain.telemetry.TableEntry
 
 class PidfController2(config: PidfConfig, val dt: Double) {
     private val kp = config.kp ?: 0.0
@@ -21,6 +25,16 @@ class PidfController2(config: PidfConfig, val dt: Double) {
     }
 }
 
-// So ktlint can be happy
-fun placeHolder() {
+fun <T> CoroutineScope.linkTableEntry(name: String, parent: Table, get: () -> T) = run {
+    val entry = TableEntry(name, get(), parent)
+    onTick {
+        entry.value = get()
+    }
+}
+
+fun <T> CoroutineScope.linkTableEntry(name: String, vararg location: String, get: () -> T) = run {
+    val entry = TableEntry(name, get(), *location)
+    onTick {
+        entry.value = get()
+    }
 }
